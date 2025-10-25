0ce5c5ac69ee9d86c52bf3a49b356741e0bf0eda
Lucifer <at2823743@gmail.com>
2025-10-25 22:23:33 +0300

    finish acount system
    Completion of the first version of the account system
    + Added a more improved profile

--- src/api/user_operation/edit_information.go
+++ src/api/user_operation/edit_information.go
@@ -1,20 +1,13 @@
 package user_operation
 
 import (
-	"database/sql"
 	"src/api/database"
-	"time"
 )
 
-func edit_discord(discord, name string) error {
+func Edit_discord(discord, name string) error {
 	db := database.InitDB()
 	defer db.Close()
 
 	_, err := db.Exec("UPDATE users SET discordID = ? WHERE username = ?;", discord, name)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
